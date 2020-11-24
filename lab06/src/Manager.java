fe44fe779a151aa51b69c625bae01cb4422e9b96
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2020-11-24 23:36:48 +0700

    Fix stupid bugs
    misunderstand statement

--- lab06/src/Manager.java
+++ lab06/src/Manager.java
@@ -2,9 +2,11 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+//import form_rt.jar;
 public class Manager {
     private ArrayList<Product> products = new ArrayList();
     public List<String> list = new ArrayList<>();
+
     public void writeToCsv() throws IOException {
         FileWriter csvWriter = new FileWriter("data.csv", false);
         csvWriter.append("Product Name");
@@ -19,6 +21,7 @@ public class Manager {
         csvWriter.flush();
         csvWriter.close();
     }
+
     public void readCsv() throws IOException {
         BufferedReader csvReader = new BufferedReader(new FileReader("data.csv"));
         String row;
@@ -34,26 +37,27 @@ public class Manager {
             System.out.println(temp.toString());
         }
     }
+
     public void removeProducts(Product temp) {
-        int index = -1;
-        for (int i = 0; i < products.size(); i++) {
-            Product p = products.get(i);
-            if (p.getProductName().equals(temp.getProductName())) {
-                index  = i;
-                break;
-            }
+        Product find = search(temp.getProductName(), temp.getDescription());
+        if (!find.getProductName().equals("NULL")) {
+            find.resizeAmount(-temp.getAmount());
+            if (find.getAmount() <= 0) products.remove(find.getIndex());
         }
-        if (index != -1)
-            products.remove(index);
     }
+
+    public void addProducts(Product temp) {
+        Product find = search(temp.getProductName(), temp.getDescription());
+        if (find.getProductName().equals("NULL")) products.add(temp);
+        else find.resizeAmount(temp.getAmount());
+    }
+
     public void updateList() {
         list.clear();
         for (Product p : products)
             list.add(p.getProductInformation());
     }
-    public void addProducts(Product temp) {
-        products.add(temp);
-    }
+
     public Product search(String name) {
         for (int i = 0; i < products.size(); i++) {
             Product p = products.get(i);
@@ -61,7 +65,18 @@ public class Manager {
                 return p;
             }
         }
-        return new Product("NULL",-1,"NULL"); // this return return when cannot find the item match name
+        return new Product("NULL", -1, "NULL"); // this return return when cannot find the item match name
+    }
+
+    private Product search(String name, String description) {
+        for (int i = 0; i < products.size(); i++) {
+            Product p = products.get(i);
+            p.setIndex(i);
+            if (p.getProductName().equals(name) && p.getDescription().equals(description)) {
+                return p;
+            }
+        }
+        return new Product("NULL", -1, "NULL");
     }
 
 
--- lab06/src/Product.java
+++ lab06/src/Product.java
@@ -1,11 +1,15 @@
 public class Product {
     private String productName, description;
-    private int amount;
+    private int amount, index;
 
     public Product(String productName, int amount, String description) {
         this.amount = amount;
         this.productName = productName;
         this.description = description;
+    }
+
+    public void resizeAmount(int num) {
+        this.amount += num;
     }
 
     public String getProductName() {
@@ -18,6 +22,14 @@ public class Product {
 
     public int getAmount() {
         return amount;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
     }
 
     @Override
